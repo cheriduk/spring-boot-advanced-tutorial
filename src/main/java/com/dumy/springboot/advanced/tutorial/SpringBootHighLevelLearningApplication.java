7bff8dba97c6751bbce79b5ec6c987aed153dd5d
AllenDu <15162678717@163.com>
2020-09-15 20:11:34 +0800

    Initial commit

---
+++ src/main/java/com/dumy/springboot/advanced/tutorial/SpringBootHighLevelLearningApplication.java
@@ -0,0 +1,13 @@
+package com.dumy.springboot.advanced.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHighLevelLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHighLevelLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dumy/springboot/advanced/tutorial/SpringBootHighLevelLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumy.springboot.advanced.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHighLevelLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
